8bda237d5257b2a6d2a9d4ca62c28617d6ba043c
Lucas-ros4 <lucasrosallrs@gmail.com>
2026-04-20 06:03:02 -0300

    corrige build Docker - adiciona COLCON_IGNORE para isolar workspace

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,22 +15,25 @@ export ROS_DOMAIN_ID=0
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /root/.bashrc
 
-# Impede o colcon de varrer pacotes Python do sistema
 touch /usr/lib/python3/dist-packages/COLCON_IGNORE
 touch /usr/local/lib/python3.10/dist-packages/COLCON_IGNORE 2>/dev/null || true
 
-# Limpa builds antigos
 rm -rf $ROS_WS/build $ROS_WS/install $ROS_WS/log
 
-# Compila apenas o pacote do projeto
 cd $ROS_WS
 colcon build \
     --packages-select controle_gestual \
     --symlink-install \
     --event-handlers console_direct+
 
+# Garante source em qualquer bash futuro
+grep -q "source /opt/ros/humble/setup.bash" /root/.bashrc || \
+    echo "source /opt/ros/humble/setup.bash" >> /root/.bashrc
+
+grep -q "source $ROS_WS/install/setup.bash" /root/.bashrc || \
+    echo "source $ROS_WS/install/setup.bash" >> /root/.bashrc
+
 source $ROS_WS/install/setup.bash
-echo "export ROS_DOMAIN_ID=0" >> /root/.bashrc
 source /root/.bashrc
 
 exec "$@"
